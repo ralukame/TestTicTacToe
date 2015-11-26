b9f8355fbe382cd7ffefde7be8ede78c4b72efd7
ralukame <raluca.stanculescu@gmail.com>
2015-11-26 21:04:16 +0200

    click on button shows x or 0

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,41 @@
  * Controller of the testApp
  */
 angular.module('testApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+        $scope.a = [
+
+    ];
+
+        $scope.currentPlayer = 'x';
+        $scope.a00 = ' ';
+        $scope.a01 = ' ';
+        $scope.a02 = ' ';
+        $scope.a10 = ' ';
+        $scope.a11 = ' ';
+        $scope.a12 = ' ';
+        $scope.a20 = ' ';
+        $scope.a21 = ' ';
+        $scope.a22 = ' ';
+
+
+        $scope.clickElement = function (pos) {
+            $scope.a00 = ' x';
+            var elem = $scope.a[pos];
+            if (elem === undefined) {
+                $scope.a[pos] = {
+                    position: pos,
+                    value: $scope.currentPlayer
+                }
+            }
+            $scope.currentPlayer = $scope.currentPlayer == 'x' ? '0' : 'x';
+        }
+
+
+
+    });
