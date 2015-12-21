5ad4a1fb38f815b2c5d2ddd69ae58edba2ae057c
ralukame <raluca.stanculescu@gmail.com>
2015-12-21 15:53:47 +0200

    use firebase services

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,40 +9,33 @@
  */
 angular.module('testApp')
     .controller('MainCtrl', function ($scope) {
-        $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
-        $scope.a = [
-
-    ];
-
+    var myDataRef = new Firebase('https://tiktactoe.firebaseio.com/');
+        $scope.a = [];
         $scope.currentPlayer = 'x';
-        $scope.a00 = ' ';
-        $scope.a01 = ' ';
-        $scope.a02 = ' ';
-        $scope.a10 = ' ';
-        $scope.a11 = ' ';
-        $scope.a12 = ' ';
-        $scope.a20 = ' ';
-        $scope.a21 = ' ';
-        $scope.a22 = ' ';
-
 
         $scope.clickElement = function (pos) {
-            $scope.a00 = ' x';
+
             var elem = $scope.a[pos];
             if (elem === undefined) {
                 $scope.a[pos] = {
                     position: pos,
                     value: $scope.currentPlayer
                 }
+                myDataRef.push({game: 1, moves: $scope.a});
             }
             $scope.currentPlayer = $scope.currentPlayer == 'x' ? '0' : 'x';
         }
 
+        $scope.reset= function () {
+        $scope.a = [];
+        $scope.currentPlayer = 'x';
+        }
+
+//          myDataRef.push({name: name, text: text});
+//           myDataRef.on('child_added', function(snapshot) {
+////  //We'll fill this in later.
+//});
 
 
     });
